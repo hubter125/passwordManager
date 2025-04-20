fccf4de9d492021f82b071cfdd7c2946b851f993
hubter <hjkorff996@mail.cfcc.edu>
2025-04-20 13:54:54 -0400

    added delete exisiting creds, need fix for removing sites safely without corrupting data

--- passwordManager.c
+++ passwordManager.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(){
     FILE *fp;
+    FILE *tmp;
     int c;
     int operand;
+    char str[100];
+    char keyword[100];
+    char line[256];
+    int skip = 0;
 
     printf("Choose an operation:\n ");
     printf("Type 1 to view stored creds:\n ");
@@ -11,19 +16,64 @@ int main(){
     printf("Type 3 to delete existing creds:\n ");
     printf("Type 4 to search for creds:\n ");
     scanf("%d", &operand );
+    getchar();
     switch (operand){
         case 1:
-            fp = fopen("password.txt", "r");
+            fp = fopen("password.txt", "r"); // open the password.txt file in Read mode to view the contents
             if (fp == NULL) {
                 printf("File does not exist\n");
                 break;
             }
-            while ((c = fgetc(fp)) != EOF)
+            while ((c = fgetc(fp)) != EOF){ // goes through until EOF and prints all characters using a while loop
                 printf("%c", c);
+            }
+            fclose(fp);
             break;
         case 2:
+            fp = fopen("password.txt", "a"); // opens password.txt in append mode, and then prompts user input
+            if (fp == NULL) {
+                printf("File does not exist\n");
+                break;
+            }
+            printf("Enter site: ");
+            fgets(str, sizeof(str), stdin);
+            fprintf(fp, "Site: %s", str);
+
+            printf("Enter Username: ");
+            fgets(str, sizeof(str), stdin);
+            fprintf(fp, "Username: %s", str);
+
+            printf("Enter Password: ");
+            fgets(str, sizeof(str), stdin);
+            fprintf(fp, "Password: %s\n", str);
+
+            fclose(fp);
             break;
         case 3:
+            fp = fopen("password.txt", "r"); // Creates a tmp file named tmp.txt, finds a keyword in password.txt, skips that line and two below,writes rest to tmp file and renames it 
+            tmp = fopen("tmp.txt", "w");
+            printf("Enter the site name to delete: ");
+            fgets(keyword, sizeof(keyword), stdin);
+            keyword[strcspn(keyword, "\n")] = 0;
+
+            while (fgets(line, sizeof(line), fp)) {
+                if (!skip && strstr(line, keyword)){
+                    skip = 3;
+                    continue;
+                }
+                if (skip > 0) {
+                    skip--;
+                    continue;
+                }
+                fputs(line, tmp);
+            }
+            fclose(fp);
+            fclose(tmp);
+
+            remove("password.txt");
+            rename("tmp.txt", "password.txt");
+            
+
             break;
         case 4:
             break;
